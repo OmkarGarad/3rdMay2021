a885e5a066133e03ae25dc8561c6f89186892f7c
Omkar Garad <omkargarad6@gmail.com>
2021-05-04 13:24:35 +0530

    add file in test04

--- src/test/java/Maven/Test04.java
+++ src/test/java/Maven/Test04.java
@@ -1,5 +1,18 @@
 package Maven;
 
-public class Test04 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class Test04 {
+	@Test (priority=1) 
+	public void verifyTitle(){
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); //WebDriver=interface
+		WebDriver driver =  new ChromeDriver();// ChromeDriver=class,RemoteWebDriver=SuperClass
+		driver.get("file:///F:/SELENIUM%20TESTING/Selenium%20Softwares/Offline%20Website/Offline%20Website/index.html");
+		String title = driver.getTitle();
+		Assert.assertEquals(title,"JavaByKiran | Log in");	
+		driver.close();
+	} 
 }
